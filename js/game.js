794fd480b4c7014a93acf55899cbcccc6b859046
Jairo Pozo <jairojairjason@gmail.com>
2021-02-09 01:33:25 -0300

    Terminada la logica del juego

--- js/game.js
+++ js/game.js
@@ -1,90 +1,79 @@
-'use strict';
-
-const game  = document.getElementById('game');
-const start = document.getElementById('game-start');
+'use strict'
+import dealCards from './dealCards.js';
+const start = document.getElementById("game-start");
 const board  = document.getElementById('game-board');
+const test = document.getElementById('game-test');
+let cards = document.getElementsByClassName("card");
+let covers = document.getElementsByClassName("cover");
 
-let icon_list = [
-"url('../assets/icon-card/christmas_icons-01.svg')",
-"url('../assets/icon-card/christmas_icons-02.svg')",
-"url('../assets/icon-card/christmas_icons-03.svg')",
-"url('../assets/icon-card/christmas_icons-04.svg')",
-"url('../assets/icon-card/christmas_icons-05.svg')",
-"url('../assets/icon-card/christmas_icons-06.svg')",
-"url('../assets/icon-card/christmas_icons-07.svg')",
-"url('../assets/icon-card/christmas_icons-08.svg')",
-"url('../assets/icon-card/christmas_icons-09.svg')",
-"url('../assets/icon-card/christmas_icons-10.svg')",
-"url('../assets/icon-card/christmas_icons-11.svg')",
-"url('../assets/icon-card/christmas_icons-12.svg')",
-];
 
-start.onclick = () => {
-    let level = 1;
-    let levelmax = 12;
-    start.style.display = 'none';
-    displayCards(board,level,levelmax);
-    test.style.display = "inline-block";
-}
-function createCard(){
-    const card = document.createElement('div');
-    card.setAttribute('id','card');
-    return card
-}
-function cardBackground(totalcards){
-    let count = 0;
-    return function assignBackground(card){
-        card.className="card";
-        let i = totalcards/2;
-        i--;
-        card.style.backgroundImage = icon_list[i];
-        count++;
-        if(count%2==0){
-            totalcards-=2;
-        }
-    }
-}
-function randomNumber(level){
-    let randomNum = [];
-    for (let index = 0; index < level; index++) {
-        let num = Math.random()*(1+level-2)+1;
-        num = num.toFixed(0);
-        if (!randomNum.includes(num)){
-            randomNum.push(num);
-        }
-        else{
-            index--;
-        }
-    }
-    return randomNum;
-}
-function dealCards(card,level){
-    let randomNum = randomNumber(level);
-    card.style.order = randomNum.pop();
-}
-function clearBoard(level){
-    if(level>1){
-        for (let index = 0; index < ((level*2)-2); index++) {
-            const card =  document.getElementById('card');
-            card.remove();
-        }
-    }
-}
-function displayCards (board,level,levelmax){
-    if(level>levelmax){
-        clearBoard(level);
-        test.style.display = "none";
-        start.style.display= "inline-block";
+function pairs (num1,num2,cover1,card1,cover2,card2){
+    if(num1 === num2){
+        return true;
     }
     else{
-        clearBoard(level);
-        let totalcards = level*2
-        let styleCards = cardBackground(totalcards);
-        for (let index = 0; index < totalcards; index++) {
-        let card = createCard();
-        styleCards(card);
-        dealCards(card,level)
-        board.appendChild(card);
-    }
+        card1.style.display='none';
+        card2.style.display='none';
+        cover1.style.display='block';
+        cover2.style.display='block';
+        return false;
     }
 }
+
+function game(memoryGame){
+    let correct = 0;
+    let attempts = [];
+    let tempCovers = [];
+    let tempCards = [];
+    if(memoryGame.getLevel()==memoryGame.getLevelMax()){
+        alert("Juego Completado!")
+        memoryGame.resetGame();
+        start.style.display = 'inline-block';
+        test.style.display='none';
+    }
+    else{
+        for (let index = 0; index < covers.length; index++) {
+            covers[index].addEventListener('click',()=>{
+                covers[index].style.display='none';
+                cards[index].style.display='block';
+                attempts.push(covers[index].getAttribute('data-index'));
+                tempCovers.push(covers[index]);
+                tempCards.push(cards[index]);
+                if(attempts.length%2==0 && attempts.length!=0){
+                    let arePairs = pairs(attempts[0],attempts[1],tempCovers[0],tempCards[0],tempCovers[1],tempCards[1]);
+                    attempts=[];
+                    tempCards=[];
+                    tempCovers=[];
+                    if(arePairs===true){
+                        correct+=2
+                        if(cards.length===correct){
+                            memoryGame.nextLevel();
+                            cards = document.getElementsByClassName("card");
+                            covers = document.getElementsByClassName("cover");
+                            game(memoryGame);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
+
+function startGame(){
+    test.style.display='none';
+    const memoryGame = dealCards(board);
+    start.onclick = () => {
+        start.style.display = 'none';
+        test.style.display='inline-block';
+        memoryGame.nextLevel();
+        game(memoryGame);
+    }
+    test.onclick = () => {
+        memoryGame.nextLevel();
+        cards = document.getElementsByClassName("card");
+        covers = document.getElementsByClassName("cover");
+        game(memoryGame);
+    }
+}
+
+startGame();
